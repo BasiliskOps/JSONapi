189c84c4497304b8c797eb2de2cb1262b7957377
BasiliskOps <noro.nero@gmail.com>
2021-03-18 14:16:19 -0500

    added port to server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 let app = require('./app')
 let server = app.listen(9000)
+app.set('port', process.env.PORT || 9000)
+let port = app.get('port')
+let server = app.listen(app.get('port'))
 
 
 server.on('listening', ()=> console.log('Server is listening'))
