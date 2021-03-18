1e1551d7f896482df0f2d4f5e5548bc22ab6ccd6
BasiliskOps <noro.nero@gmail.com>
2021-03-18 14:27:09 -0500

    more server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 let app = require('./app')
 
-app.set('port', process.env.PORT || 9000)
-let port = app.get('port')
+// app.set('port', process.env.PORT || 9000)
 let server = app.listen(app.get('port'))
+const PORT = process.env.PORT || '9000'
 
 
+app.set('port', PORT)
 server.on('listening', ()=> console.log('Server is listening'))
 server.on('error', ()=> console.error('Server is dead'))
 
