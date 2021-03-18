d581947c242a7b00d566a85a2f0d14a8d3f5a97a
BasiliskOps <noro.nero@gmail.com>
2021-03-18 14:32:00 -0500

    server.js still getting PORT error

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 let app = require('./app')
 
-// app.set('port', process.env.PORT || 9000)
+app.set('port', process.env.PORT || 9000)
+
 let server = app.listen(app.get('port'))
-const PORT = process.env.PORT || '9000'
 
 
-app.set('port', PORT)
 server.on('listening', ()=> console.log('Server is listening'))
 server.on('error', ()=> console.error('Server is dead'))
 
