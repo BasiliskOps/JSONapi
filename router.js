0a46a9c9853579ae5ae8ea3aec2a6d1ae1f87271
BasiliskOps <noro.nero@gmail.com>
2021-03-22 15:01:48 -0500

    setting index redirect

--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 let express = require('express')
 
 let coronaRouter = require('./INNER SANCTUM/router')
@@ -5,5 +6,5 @@ let coronaRouter = require('./INNER SANCTUM/router')
 let router = new express.Router()
 
 router.use('/coronas', coronaRouter)
-
+router.get('/', (request, response)=> response.redirect('/coronas/'))
 module.exports = router
