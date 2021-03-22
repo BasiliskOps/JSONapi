e8feba9229777c51098d760f3ed9c17b41d34dd5
BasiliskOps <noro.nero@gmail.com>
2021-03-22 13:30:12 -0500

    reseeded data

--- seed.js
+++ seed.js
@@ -1,9 +1,16 @@
 require('./connection.js')
 let mongoose = require("mongoose")
 let coronaModel = require("./INNER SANCTUM/model")
+let countryModel = require("./cmodel")
 let websiteData = require("./corona.json")
 coronaModel.deleteMany({}).then(
   coronaModel.create(websiteData).then((coronas) => {
+    console.log(coronas.length, "Corona data established")
+    mongoose.disconnect()
+  })
+);
+countryModel.deleteMany({}).then(
+  countryModel.create(websiteData).then((coronas) => {
     console.log(coronas.length, "Corona data established")
     mongoose.disconnect()
   })
