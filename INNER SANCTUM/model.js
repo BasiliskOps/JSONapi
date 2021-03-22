8e2c7d831f40dee32767bb3d2c6110da50104e3e
BasiliskOps <noro.nero@gmail.com>
2021-03-22 11:56:28 -0500

    reverted model.js back to original

--- INNER SANCTUM/model.js
+++ INNER SANCTUM/model.js
@@ -1,38 +1,33 @@
 let mongoose = require('mongoose')
 
-let globalSchema = new mongoose.Schema({
-    Global: {
-        Countries: String,
-        NewConfirmed: String,
-        TotalDeaths: Number,
-        NewDeaths: Number,
-        TotalDeaths: Number,
-        NewRecovered: String,
-        TotalRecovered: Number,
-        Date: Date
-    }
+let coronaSchema = new mongoose.Schema({
+   Global: {
+    NewConfirmed: String,
+    TotalDeaths: Number,
+    NewDeaths: Number,
+    TotalDeaths: Number,
+    NewRecovered: String,
+    TotalRecovered: Number,
+    Date: Date
+    },
+    Countries: 
+        {
+            Country: String,
+            NewConfirmed: Number,
+            TotalConfirmed: Number,
+            NewDeaths: Number,
+            TotalDeaths: Number,
+            NewRecovered: Number,
+            TotalRecovered: Number,
+            Date: Date
+        } 
+    
 
-})
-
-let countrySchema = new mongoose.Schema({
-    Countries: [{
-        Name: String,
-        NewConfirmed: Number,
-        TotalConfirmed: Number,
-        NewDeaths: Number,
-        TotalDeaths: Number,
-        NewRecovered: Number,
-        TotalRecovered: Number,
-        Date: Date
-    }]
-        
-
-
+     
 
 
 })
 
-let Global = mongoose.model('Global', globalSchema)
-let Country = mongoose.model('Country', countrySchema)
+let Corona = mongoose.model('Corona', coronaSchema)
 
-module.exports = Global, Country
+module.exports = Corona
