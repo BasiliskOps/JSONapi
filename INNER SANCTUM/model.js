08b7aac68a5f06cbd764a0f1bef95dd71d5451bb
BasiliskOps <noro.nero@gmail.com>
2021-03-22 10:42:26 -0500

    rework model.js

--- INNER SANCTUM/model.js
+++ INNER SANCTUM/model.js
@@ -1,32 +1,11 @@
 let mongoose = require('mongoose')
 
-let globalSchema = new mongoose.Schema({
-  
-    NewConfirmed: String,
-    TotalDeaths: Number,
-    NewDeaths: Number,
-    TotalDeaths: Number,
-    NewRecovered: String,
-    TotalRecovered: Number,
-    Date: Date
-    
+let countrySchema = new mongoose.Schema({
+    name: String,
+    totalDeaths: Number,
+    totalRecovered: Number
 })
 
-let countrySchema = new mongoose.Schema ({
- 
-            Name: String,
-            NewConfirmed: Number,
-            TotalConfirmed: Number,
-            NewDeaths: Number,
-            TotalDeaths: Number,
-            NewRecovered: Number,
-            TotalRecovered: Number,
-            Date: Date,
-        
-    
+let Country = mongoose.model('Country',  countrySchema)
 
-})
-
-let Corona = mongoose.model('Corona', globalSchema, countrySchema)
-
-module.exports = Corona
+module.exports = Country
